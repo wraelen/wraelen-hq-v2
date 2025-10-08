b19dd4e2ae6de5590034a16d2b48794344be8073
wraelen <wraelen@gmail.com>
2025-10-08 18:44:52 -0400

    grokly68

--- .next/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/node_modules__pnpm_5a9c9b9b._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__fa248b9f._.js")
-R.m("[project]/node_modules/.pnpm/next@15.5.4_@babel+core@7.27.1_@opentelemetry+api@1.7.0_@playwright+test@1.52.0_babel-p_68e8c185df7d969f063bfb2ef00a51ed/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/.pnpm/next@15.5.4_@babel+core@7.27.1_@opentelemetry+api@1.7.0_@playwright+test@1.52.0_babel-p_68e8c185df7d969f063bfb2ef00a51ed/node_modules/next/document.js [ssr] (ecmascript)").exports
